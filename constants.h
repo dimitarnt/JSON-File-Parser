2e2bf2869e9cef3eb4d16dde923cf41abf0b8406
Dimitar Nikolaev Tankov <dimt.nikolaev@gmail.com>
2023-05-08 00:13:45 +0300

    Adding header file with constants.

---
+++ constants.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+const int NUMBER_OF_BYTES_IN_CHAR_PTR = sizeof(char*);
+const int NUMBER_OF_BYTES_IN_SIZE_T = sizeof(size_t);
+
+const int MAX_SIZE_T_DIGITS = 20;
+const int MAX_UNSIGNED_INT_DIGITS = 10;
+
+const int BUFFER_SIZE = 1024;
+
+const unsigned char BITS[] = { 128, 64, 32, 16, 8, 4, 2, 1 };
