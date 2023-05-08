2825af491143b3db4d4800b05d3444d6f6964771
Dimitar Nikolaev Tankov <dimt.nikolaev@gmail.com>
2023-05-08 23:32:24 +0300

    Moved certain functions to other files.

--- helperFunctions.h
+++ helperFunctions.h
@@ -7,13 +7,3 @@ long long pow(int num, size_t n);
 size_t numberOfDigits(size_t num);
 
 char getDigit(size_t num, size_t index, size_t digitCount);
-
-unsigned getFileSize(std::ifstream& file);
-
-unsigned getCharCountFromFile(std::ifstream& in, char symbol);
-
-unsigned getCharCountFromFile(std::ifstream& in, char symbol, unsigned untilPosition);
-
-unsigned getLinesCount(std::ifstream& in);
-
-unsigned getLinesCount(std::ifstream& in, unsigned untilPosition);
