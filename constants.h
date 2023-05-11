a8008b91575e5464ad04a9ffc1d3596bf1a8d091
Dimitar Nikolaev Tankov <dimt.nikolaev@gmail.com>
2023-05-11 23:17:59 +0300

    Added a few constant values.

--- constants.h
+++ constants.h
@@ -4,6 +4,9 @@
 const int NUMBER_OF_BYTES_IN_CHAR_PTR = sizeof(char*);
 const int NUMBER_OF_BYTES_IN_SIZE_T = sizeof(size_t);
 
+const int NUMBER_OF_DIGITS = 10;
+const int NUMBER_OF_JSON_KEYWORDS = 3;
+
 const char* const JSON_FILE_EXTENSION = ".json";
 
 const int MAX_SIZE_T_DIGITS = 20;
