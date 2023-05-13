1dd8f49f5d8a76f95f5736612ccd9e81bc71acf7
Dimitar Nikolaev Tankov <dimt.nikolaev@gmail.com>
2023-05-13 17:43:03 +0300

    Changed names of getline type functions.

--- fileFunctions.h
+++ fileFunctions.h
@@ -5,11 +5,10 @@
 unsigned getFileSize(std::ifstream& file);
 
 unsigned getCharCountFromFile(std::ifstream& in, char symbol);
-
 unsigned getCharCountFromFile(std::ifstream& in, char symbol, unsigned untilPosition);
 
 unsigned getLinesCount(std::ifstream& in);
-
 unsigned getLinesCount(std::ifstream& in, unsigned untilPosition);
 
-void getLineForString(std::ifstream& in, String& str, unsigned bufferSize, char delimiter);
+void getCharactersUntilDelimiter(std::ifstream& in, String& str, unsigned bufferSize, char delimiter);
+void getCharactersUntilDelimiter(std::ifstream& in, String& str, unsigned bufferSize, char delimiter1, char delimiter2, char delimiter3);
