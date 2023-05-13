ba0e05af81261d99ea503d23767e74bcdf3f4827
Dimitar Nikolaev Tankov <dimt.nikolaev@gmail.com>
2023-05-13 19:54:39 +0300

    Adding main class for parsing, containing the starting node.

---
+++ JsonParser.cpp
@@ -0,0 +1,44 @@
+#include "JsonParser.h"
+#include "JsonObject.h"
+#include "JsonArray.h"
+#include "constants.h"
+
+void JsonParser::assertJsonFileName(const char* fileName) const {
+    String name(fileName);
+    String extension(JSON_FILE_EXTENSION);
+    size_t extensionLength = extension.getLength();
+
+    if(name.substring(name.getLength() - extensionLength, extensionLength) != extension) {
+        throw std::invalid_argument("File name does not have a JSON file extension");
+    }
+}
+
+JsonParser::JsonParser(const char* fileName) {
+    setFile(fileName);
+}
+
+void JsonParser::setFile(const char* fileName) {
+    assertJsonFileName(fileName);
+
+    std::ifstream in(fileName);
+
+    if(!in.is_open()) {
+        throw std::ifstream::failure("Unable to open file");
+    }
+
+    JsonValidator fileValidator(in);
+
+    fileValidator.validate();
+
+    char firstChar = (char)in.get();
+
+    if(firstChar == '{') {
+        JsonObject startingObject(in);
+        _startingNode = startingObject.clone();
+    }
+
+    if(firstChar == '[') {
+        JsonArray startingArray(in);
+        _startingNode = startingArray.clone();
+    }
+}
---
+++ JsonParser.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "JsonValidator.h"
+#include "JsonNode.h"
+
+class JsonParser {
+private:
+    String _fileName;
+    JsonNode* _startingNode;
+
+    void assertJsonFileName(const char* fileName) const;
+
+public:
+    explicit JsonParser(const char* fileName);
+
+    void setFile(const char* fileName);
+
+};
