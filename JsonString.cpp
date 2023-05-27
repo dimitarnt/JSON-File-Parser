a2ba6df6c9f88b66dfbe6b7f1671af6c459c2c90
Dimitar Nikolaev Tankov <dimt.nikolaev@gmail.com>
2023-05-27 14:00:40 +0300

    Added new constructors.

--- JsonString.cpp
+++ JsonString.cpp
@@ -4,6 +4,10 @@ JsonString::JsonString(std::ifstream& in) : JsonNode(JsonNodeType::JSON_STRING) 
     _value = parseValue(in);
 }
 
+JsonString::JsonString(const String& value) : JsonNode(JsonNodeType::JSON_STRING), _value(value) {}
+
+JsonString::JsonString(String&& value) : JsonNode(JsonNodeType::JSON_STRING), _value(std::move(value)) {}
+
 void JsonString::print(unsigned nestingLevel, bool isInArray) const {
     std::cout << '\"' << _value << '\"';
 }
--- JsonString.h
+++ JsonString.h
@@ -9,6 +9,8 @@ private:
 public:
     JsonString() = delete;
     explicit JsonString(std::ifstream& in);
+    explicit JsonString(const String& value);
+    explicit JsonString(String&& value);
 
     void print(unsigned nestingLevel, bool isInArray) const override;
 
