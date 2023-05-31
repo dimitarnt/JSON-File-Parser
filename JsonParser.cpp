3fc629c32015a98ddae52c4f8bed0218f9a500e3
Dimitar Nikolaev Tankov <dimt.nikolaev@gmail.com>
2023-06-01 00:50:38 +0300

    Added create functions.

--- JsonParser.cpp
+++ JsonParser.cpp
@@ -1,7 +1,8 @@
 #include "JsonParser.h"
-#include "JsonObject.h"
 #include "JsonArray.h"
+#include "InvalidJsonSyntax.h"
 #include "constants.h"
+#include <cstring>
 
 void JsonParser::assertJsonFileName(const char* fileName) {
     String name(fileName);
@@ -56,8 +57,34 @@ void JsonParser::search(const char* key) const {
     std::cout << std::endl;
 }
 
+void JsonParser::assertString(const char* str) {
+    size_t strLength = strlen(str);
+
+    if(getCharCount(str, strLength, '\"') != 0 || getCharCount(str, strLength, '\n') != 0) {
+        throw InvalidJsonSyntax("Disallowed character in given string");
+    }
+}
+
 void JsonParser::set(const char* path, const char* newStr) {
+    assertString(newStr);
+
     _startingNode[0]->set(path, newStr, 0);
+}
+
+void JsonParser::createInArray(const char* path, const char* newStr) {
+    create(path, false, true, "", newStr);
+}
+
+void JsonParser::createInObject(const char* newKey, const char* newStr) {
+    create("", true, false, newKey, newStr);
+}
+
+void JsonParser::createInObject(const char* path, const char* newKey, const char* newStr) {
+    create(path, false, false, newKey, newStr);
+}
+
+void JsonParser::create(const char* path, bool isAddressingStartingNode, bool createInArray, const char* newKey, const char* newStr) {
+    _startingNode[0]->create(path, isAddressingStartingNode, createInArray, newKey, newStr, 0);
 }
 
 void JsonParser::remove(const char* path) {
--- JsonParser.h
+++ JsonParser.h
@@ -8,6 +8,9 @@ private:
     JsonNodeCollection _startingNode;
 
     static void assertJsonFileName(const char* fileName);
+    static void assertString(const char* str);
+
+    void create(const char* path, bool isAddressingStartingNode, bool createInArray, const char* newKey, const char* newStr);
 
 public:
     JsonParser() = delete;
@@ -19,5 +22,8 @@ public:
     void search(const char* key) const;
 
     void set(const char* path, const char* newStr);
+    void createInArray(const char* path, const char* newStr);
+    void createInObject(const char* newKey, const char* newStr);
+    void createInObject(const char* path, const char* newKey, const char* newStr);
     void remove(const char* path);
 };
