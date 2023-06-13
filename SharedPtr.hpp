c10a55fb9124891b0931be5e4b9f2b2f116850a0
Dimitar Nikolaev Tankov <dimt.nikolaev@gmail.com>
2023-06-13 17:17:52 +0300

    Added new features. Move constructor, move operator=, get, reset, getCount and overloaded operator bool functions.

--- SharedPtr.hpp
+++ SharedPtr.hpp
@@ -1,98 +1,85 @@
 #pragma once
-#include <iostream>
 
-template <typename T>
+//source - https://github.com/GeorgiTerziev02/Object-oriented_programming_FMI/tree/main/Sem.%2013/Solutions/SharedPointer
+
+template<typename T>
 class SharedPtr {
-    T* data;
-    unsigned* pointersCount;
-
-    void free();
-    void copyFrom(const SharedPtr<T>& other);
+private:
+    T* data = nullptr;
+    size_t* counter = nullptr;
 
 public:
-    SharedPtr();
-    SharedPtr(T* data);
-
+    SharedPtr(T* data = nullptr);
     SharedPtr(const SharedPtr<T>& other);
-    SharedPtr& operator=(const SharedPtr<T>& other);
+    SharedPtr(SharedPtr<T>&& other) noexcept;
+    SharedPtr<T>& operator=(const SharedPtr<T>& other);
+    SharedPtr<T>& operator=(SharedPtr<T>&& other) noexcept;
+    ~SharedPtr();
 
-    const T& operator*() const;
-    T& operator*();
     const T* operator->() const;
     T* operator->();
+    const T& operator*() const;
+    T& operator*();
 
-    ~SharedPtr();
+    T* get() const;
+    void reset(T* data = nullptr);
+    size_t getCount() const;
+
+    explicit operator bool() const;
+
+private:
+    void copyFrom(const SharedPtr<T>& other);
+    void moveFrom(SharedPtr<T>&& other) noexcept;
+    void free();
 };
 
-template <typename T>
-void SharedPtr<T>::free() {
-    if(data == nullptr && pointersCount == nullptr) {
-        return;
-    }
 
-    if (*pointersCount == 1) {
-
-        delete data;
-        delete pointersCount;
-    } else {
-
-        (*pointersCount)--;
-    }
-}
-
-template <typename T>
-void SharedPtr<T>::copyFrom(const SharedPtr<T>& other) {
-
-    data = other.data;
-    pointersCount = other.pointersCount;
-    (*pointersCount)++;
-}
-
-template <typename T>
-SharedPtr<T>::SharedPtr() {
-    data = nullptr;
-    pointersCount = nullptr;
-}
-
-template <typename T>
+template<typename T>
 SharedPtr<T>::SharedPtr(T* data) {
     this->data = data;
-    pointersCount = new unsigned(1);
+    if (this->data) {
+        counter = new size_t(1);
+    }
 }
 
-template <typename T>
+template<typename T>
 SharedPtr<T>::SharedPtr(const SharedPtr<T>& other) {
     copyFrom(other);
 }
 
-template <typename T>
+template<typename T>
+SharedPtr<T>::SharedPtr(SharedPtr<T>&& other) noexcept {
+    moveFrom(std::move(other));
+}
+
+template<typename T>
 SharedPtr<T>& SharedPtr<T>::operator=(const SharedPtr<T>& other) {
     if (this != &other) {
         free();
         copyFrom(other);
     }
+
     return *this;
 }
 
-template <typename T>
-const T& SharedPtr<T>::operator*() const {
-    if(data == nullptr) {
-        throw std::runtime_error("Pointer not set");
+template<typename T>
+SharedPtr<T>& SharedPtr<T>::operator=(SharedPtr<T>&& other) noexcept {
+    if (this != &other) {
+        free();
+        moveFrom(std::move(other));
     }
-    return *data;
+
+    return *this;
 }
 
-template <typename T>
-T& SharedPtr<T>::operator*() {
-    if(data == nullptr) {
-        throw std::runtime_error("Pointer not set");
-    }
-    return *data;
-}
-
-template <typename T>
+template<typename T>
 SharedPtr<T>::~SharedPtr() {
     free();
+}
+
+template<typename T>
+const T* SharedPtr<T>::operator->() const {
+    return data;
 }
 
 template<typename T>
@@ -101,6 +88,72 @@ T* SharedPtr<T>::operator->() {
 }
 
 template<typename T>
-const T* SharedPtr<T>::operator->() const {
+const T& SharedPtr<T>::operator*() const {
+    return *data;
+}
+
+template<typename T>
+T& SharedPtr<T>::operator*() {
+    return *data;
+}
+
+template<typename T>
+T* SharedPtr<T>::get() const {
     return data;
+}
+
+template<typename T>
+void SharedPtr<T>::reset(T* dataPtr) {
+    if (this->data == data) {
+        return;
+    }
+
+    free();
+    this->data = data;
+    if (this->data) {
+        counter = new size_t(1);
+    }
+}
+
+template<typename T>
+size_t SharedPtr<T>::getCount() const {
+    return *counter;
+}
+
+template<typename T>
+SharedPtr<T>::operator bool() const {
+    return data;
+}
+
+template<typename T>
+void SharedPtr<T>::copyFrom(const SharedPtr<T>& other) {
+    data = other.data;
+    counter = other.counter;
+    if (data) {
+        (*counter)++;
+    }
+}
+
+template<typename T>
+void SharedPtr<T>::moveFrom(SharedPtr<T>&& other) noexcept {
+    data = other.data;
+    counter = other.counter;
+
+    other.data = nullptr;
+    other.counter = nullptr;
+}
+
+template<typename T>
+void SharedPtr<T>::free() {
+    if (!data) {
+        return;
+    }
+
+    if ((*counter) == 1) {
+        delete data;
+        delete counter;
+    }
+    else {
+        (*counter)--;
+    }
 }
